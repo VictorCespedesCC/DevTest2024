87d11c7ee9b28d806d4f0ad658a5ead31143c69b
VictorCespedesCC <victor.cespedes.9374448@usalesiana.edu.bo>
2024-11-09 12:12:02 -0400

    final changes

--- devTest/src/main/java/com/victor/devTest/Controller/PollController.java
+++ devTest/src/main/java/com/victor/devTest/Controller/PollController.java
@@ -4,7 +4,6 @@ import com.victor.devTest.Model.Option;
 import com.victor.devTest.Model.Poll;
 import com.victor.devTest.Model.Vote;
 import com.victor.devTest.Service.PollService;
-import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,19 +12,19 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/bol")
+@RequestMapping("/api/v1/Poll")
 public class PollController {
 
     @Autowired
     private PollService pollService;
 
 
-    @GetMapping("/v1/polls")
+    @GetMapping
     public List<Poll> getAllPolls(){
         return pollService.getPolls();
     }
 
-    @PostMapping("/v1/polls")
+    @PostMapping("/api/v1/polls")
     public ResponseEntity<Poll> createPoll(@RequestBody Map<String, Object> body){
         String name = (String) body.get("name");
         Option[] options = (Option[]) body.get("options");
@@ -35,7 +34,7 @@ public class PollController {
 
     }
 
-    @PostMapping("/v1/polls/{:id}/votes")
+    @PostMapping("api/v1/polls/{:id}/votes")
     public ResponseEntity<Vote> voteForPoll(@PathVariable int pollId, Map<String, Object> body){
         Vote vote = pollService.vote(pollId,(int)body.get("optionId"), (String)body.get("voterEmail"));
         return ResponseEntity.ok(vote);
